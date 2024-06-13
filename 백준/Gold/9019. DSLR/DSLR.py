b26ef20aef71f5b63f6091011c804daccb689ef8
이범기 <60038592+baaaam771@users.noreply.github.com>
2024-06-13 22:38:27 +0900

    [Gold IV] Title: DSLR, Time: 3264 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.py
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -1,39 +1,54 @@
 import sys
 from collections import deque
 
-t = int(input())
 
-for _ in range(t):
-    a, b = map(int,sys.stdin.readline().rstrip().split())
-    visited = [False] * 10000
+def solve(A, B):
+    visited1 = [ "" ] * 10000
+    visited2 = [ "" ] * 10000
+    queue = deque()
+    queue.append((A, 1))
+    queue.append((B, 2))
+    
+    while queue:
+        cur, type = queue.popleft()
+        
+        if type == 1:
+            operations = [
+                ((2 * cur) % 10_000, 'D'),
+                ((cur - 1) % 10000, 'S'),
+                (((cur % 1000) * 10 + (cur//1000)), 'L'),
+                (((cur % 10) * 1000 + (cur // 10)), 'R'),
+            ]
+            
+            for next, operation in operations:
+                if visited1[next] != "" or next == cur: continue
+                queue.append((next, type))
+                visited1[next] = ''.join((visited1[cur], operation))
+                
+                if next == B: return visited1[next]
+                elif visited1[next] != "" and visited2[next] != "":
+                    return ''.join((visited1[next], visited2[next]))
 
-    q = deque()
-    q.append((a, ''))
-    visited[a] = True
+        else: # 역방향 탐색
+            operations = [
+                (cur // 2, 'D'),
+                ((cur + 10000) // 2, 'D'),
+                (0 if cur == 9999 else cur + 1, 'S'),
+                (((cur % 10) * 1000 + (cur // 10)), 'L'),
+                (((cur % 1000) * 10 + (cur//1000)), 'R'),
+            ]
+            
+            for next, operation in operations:
+                if visited2[next] != "" or next == cur: continue
+                if operation == "D" and cur % 2 != 0: continue
+                queue.append((next, type))
+                visited2[next] = ''.join((operation, visited2[cur]))
+                
+                if next == A: return visited2[next]
+                elif visited1[next] != "" and visited2[next] != "":
+                    return ''.join((visited1[next], visited2[next]))
+            
 
-    while q:
-        num, oper = q.popleft()
-
-        if num == b:
-            print(oper)
-            break
-
-        d = num * 2 % 10000
-        if not visited[d]:
-            visited[d] = True
-            q.append((d, oper + 'D'))
-
-        s = (num - 1) % 10000
-        if not visited[s]:
-            visited[s] = True
-            q.append((s, oper + 'S'))
-
-        l = (num % 1000) * 10 + num // 1000
-        if not visited[l]:
-            visited[l] = True
-            q.append((l, oper + 'L'))
-
-        r = (num % 10) * 1000 + num // 10
-        if not visited[r]:
-            visited[r] = True
-            q.append((r, oper + 'R'))
+for T in range(int(input())):
+    A, B = map(int,sys.stdin.readline().rstrip().split())
+    print(solve(A, B))
