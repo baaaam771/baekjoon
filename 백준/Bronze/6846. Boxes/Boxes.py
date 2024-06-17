e03aeb9bb72cdd533202cef7ccd220dd8e4abb6d
이범기 <60038592+baaaam771@users.noreply.github.com>
2024-06-17 22:57:59 +0900

    [Bronze I] Title: Boxes, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/6846. Boxes/Boxes.py
+++ 백준/Bronze/6846. Boxes/Boxes.py
@@ -1,26 +1,18 @@
-import itertools
-
-# 입력을 받습니다.
 n = int(input())
-std_boxes = [sorted(list(map(int, input().split()))) for _ in range(n)]
+std = []
+for _ in range(n):
+    std.append(sorted(list(map(int, input().split()))))
+std.sort(key=lambda box: box[0] * box[1] * box[2])
+
 m = int(input())
-items = [list(map(int, input().split())) for _ in range(m)]
-
-# 가능한 모든 회전 형태를 생성하는 함수
-def get_rotations(item):
-    return set(itertools.permutations(item))
-
-# 각 아이템에 대해 적합한 박스를 찾고 결과를 출력합니다.
-for item in items:
-    item_rotations = get_rotations(item)
-    min_volume = float('inf')
-    fits = False
-    for box in std_boxes:
-        for rotation in item_rotations:
-            if all(rotation[i] <= box[i] for i in range(3)):
-                fits = True
-                min_volume = min(min_volume, box[0] * box[1] * box[2])
-    if fits:
-        print(min_volume)
-    else:
-        print("Item does not fit.")
+for _ in range(m):
+    temp = sorted(list(map(int, input().split())))
+    cnt = 0
+    for i in range(n):
+        if std[i][0] >= temp[0] and std[i][1] >= temp[1] and std[i][2] >= temp[2]:
+            print(std[i][0] * std[i][1] * std[i][2])
+            break
+        else:
+            cnt += 1
+    if cnt == n:
+        print('Item does not fit.')
