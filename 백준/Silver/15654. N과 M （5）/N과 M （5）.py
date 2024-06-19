def8b2f90eef0b763cdfd028304384cfb47338b8
이범기 <60038592+baaaam771@users.noreply.github.com>
2024-06-19 21:18:11 +0900

    [Silver III] Title: N과 M (5), Time: 156 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,5 +1,19 @@
-import itertools
 n, m = map(int, input().split())
-arr = sorted(list(set(map(int, input().split()))))
-for s in itertools.permutations(arr, m):
-    print(*s)
+arr = sorted(list(map(int, input().split())))
+nums = []
+chk = [0] * 10001
+
+def back(dep, point):
+    if dep == m:
+        print(*nums)
+        return
+    
+    for i in arr:
+        if chk[i] == 0:
+            chk[i] = 1
+            nums.append(i)
+            back(dep + 1, i)
+            chk[i] = 0
+            nums.pop()
+
+back(0, arr[0])
