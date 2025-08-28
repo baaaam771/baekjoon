4be9d515600faa0cd7d8fb80ae030621f57e31db
Lee, BeomGi <60038592+baaaam771@users.noreply.github.com>
2025-08-28 12:05:12 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,20 +1,8 @@
-heights = []
-for _ in range(9):
-    heights.append(int(input()))
+from itertools import combinations
+arr = [int(input()) for _ in range(9)]
+arr.sort()
 
-# print(heights)
-
-for i in range(9):
-    for j in range(i+1, 9):
-        # print(i, j)
-        new = heights[:]
-        del new[i]
-        del new[j-1]
-        # print(new)
-        # print(heights)
-        all = sum(new)
-        # print(all)
-        if all == 100:
-            arrange = sorted(new)
-            print(*arrange, sep='\n')
-            exit(0)
+for i in combinations(arr, 7):
+    if sum(list(i)) == 100:
+        print(*i, sep = "\n")
+        break
