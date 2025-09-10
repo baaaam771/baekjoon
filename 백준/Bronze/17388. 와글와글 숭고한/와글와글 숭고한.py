59a5cb2c26e7a557cea0038523f0e3d23714214f
Lee, BeomGi <60038592+baaaam771@users.noreply.github.com>
2025-09-10 19:18:28 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.py
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.py
@@ -1,15 +1,11 @@
 S, K, H = map(int, input().split())
-tot = []
-tot.append(S)
-tot.append(K)
-tot.append(H)
 
-if sum(tot) >= 100:
+if S + K + H >= 100:
     print("OK")
 else:
-    if min(tot) == S:
+    if min(S, K, H) == S:
         print("Soongsil")
-    elif min(tot) == K:
+    elif min(S, K, H) == K:
         print("Korea")
     else:
         print("Hanyang")
